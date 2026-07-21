55f067ae5995b1a0f884446a7154ab6b0c09a1c6
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2026-07-21 19:39:56 +0700

    v0.5

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
 # Proximity Sensor Disabler Magisk Module
 
 ## Descriptions
-- Disables proximity sensor or disables proximity fusion sensor or removes android.hardware.sensor.proximity feature systemlessly.
+Disables proximity sensor or disables proximity fusion sensor or removes android.hardware.sensor.proximity feature systemlessly.
+
+## Changelog
+
+v0.5
+- Resets module folders/files permissions at post-fs-data
+
+v0.4
+- Add ro.sony.sensors.proximity support
+- Does not remove android.hardware.sensor.proximity feature if ro.*.sensors.proximity is found
+
+v0.3
+- Removes android.hardware.sensor.proximity feature systemlessly
+- Saves log while installing via recovery if Magisk installed
+- Fix conflict with modules_update while installing via recovery if Magisk installed
+- Add new optional
+
+v0.2
+- Fixes proximity sensor issue instead in some ROMs
+
+v0.1
+- Initial release
 
 ## Requirements
-- Magisk or Kitsune Mask or KernelSU or Apatch installed
+Magisk or Kitsune Mask or KernelSU or Apatch installed
 
 ## Installation Guide & Download Link
-- Install this module https://www.pling.com/p/2100679/ via Magisk app or Kitsune Mask app or KernelSU app or Apatch app or Recovery if Magisk or Kitsune Mask installed
+- Install this module https://github.com/reiryuki/Proximity-Sensor-Disabler-Magisk-Module via Magisk app or Kitsune Mask app or KernelSU app or Apatch app or Recovery if Magisk or Kitsune Mask installed
 - Reboot
 
 ## Optionals
@@ -15,7 +36,7 @@
 - Global: https://t.me/ryukinotes/35
 
 ## Troubleshootings
-- Global: https://t.me/ryukinotes/34
+Global: https://t.me/ryukinotes/34
 
 ## Support & Bug Report
 - https://t.me/ryukinotes/54
@@ -23,9 +44,9 @@
 
 ## Credits and Contributors
 - https://t.me/androidryukimodsdiscussions
-- You can contribute ideas about this Magisk Module here: https://t.me/androidappsportdevelopment
+- https://t.me/androidappsportdevelopment
 
 ## Sponsors
-- https://t.me/ryukinotes/25
+https://t.me/ryukinotes/25
 
 
--- function.sh
+++ function.sh
@@ -1,4 +1,7 @@
 # function
+apex_list() {
+APXS=`ls -dp /apex/* | grep '/$' | sed -e 's|/$||' -e 's|^/apex/sharedlibs$||'`
+}
 mount_partitions_in_recovery() {
 if [ "$BOOTMODE" != true ]; then
   BLOCK=/dev/block/bootdevice/by-name
@@ -13,50 +16,26 @@ if [ "$BOOTMODE" != true ]; then
     || mount -o rw -t auto $BLOCK/cust $DIR\
     || mount -o rw -t auto $BLOCK2/cust $DIR
   fi
-  DIR=/product
-  if [ -d $DIR ] && ! is_mounted $DIR; then
-    mount -o rw -t auto $BLOCK$DIR$SLOT $DIR\
-    || mount -o rw -t auto $BLOCK2$DIR$SLOT $DIR
-  fi
-  DIR=/system_ext
-  if [ -d $DIR ] && ! is_mounted $DIR; then
-    mount -o rw -t auto $BLOCK$DIR$SLOT $DIR\
-    || mount -o rw -t auto $BLOCK2$DIR$SLOT $DIR
-  fi
-  DIR=/odm
-  if [ -d $DIR ] && ! is_mounted $DIR; then
-    mount -o rw -t auto $BLOCK$DIR$SLOT $DIR\
-    || mount -o rw -t auto $BLOCK2$DIR$SLOT $DIR
-  fi
-  DIR=/my_product
-  if [ -d $DIR ] && ! is_mounted $DIR; then
-    mount -o rw -t auto $BLOCK$DIR $DIR\
-    || mount -o rw -t auto $BLOCK2$DIR $DIR
-  fi
+  DIRS="/product /system_ext /odm"
+  for DIR in $DIRS; do
+    if [ -d $DIR ] && ! is_mounted $DIR; then
+      mount -o rw -t auto $BLOCK$DIR$SLOT $DIR\
+      || mount -o rw -t auto $BLOCK2$DIR$SLOT $DIR
+    fi
+  done
+#  apex_list
+  DIRS="/my_product /cache /persist /metadata /cust /klogdump
+        $APXS"
+  for DIR in $DIRS; do
+    if [ -d $DIR ] && ! is_mounted $DIR; then
+      mount -o rw -t auto $BLOCK$DIR $DIR\
+      || mount -o rw -t auto $BLOCK2$DIR $DIR
+    fi
+  done
   DIR=/data
   if [ -d $DIR ] && ! is_mounted $DIR; then
     mount -o rw -t auto $BLOCK/userdata $DIR\
     || mount -o rw -t auto $BLOCK2/userdata $DIR
-  fi
-  DIR=/cache
-  if [ -d $DIR ] && ! is_mounted $DIR; then
-    mount -o rw -t auto $BLOCK$DIR $DIR\
-    || mount -o rw -t auto $BLOCK2$DIR $DIR
-  fi
-  DIR=/persist
-  if [ -d $DIR ] && ! is_mounted $DIR; then
-    mount -o rw -t auto $BLOCK$DIR $DIR\
-    || mount -o rw -t auto $BLOCK2$DIR $DIR
-  fi
-  DIR=/metadata
-  if [ -d $DIR ] && ! is_mounted $DIR; then
-    mount -o rw -t auto $BLOCK$DIR $DIR\
-    || mount -o rw -t auto $BLOCK2$DIR $DIR
-  fi
-  DIR=/cust
-  if [ -d $DIR ] && ! is_mounted $DIR; then
-    mount -o rw -t auto $BLOCK$DIR $DIR\
-    || mount -o rw -t auto $BLOCK2$DIR $DIR
   fi
 fi
 }
